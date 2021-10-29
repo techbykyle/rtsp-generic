7e7d5ed0453721e882228676576985b7acf49a4e
tbk <kyle@techbykyle.com>
2021-10-29 16:08:40 -0700

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import RtspGeneric from './components/RtspGeneric'
+
+const App = () => {
+	return <RtspGeneric />
+}
+
+export default App
---
+++ src/components/RtspGeneric.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+
+const RtspGeneric = ({clearTileState, device, tile, useRtsp, useRtspPlayStream}) => {
+
+    const device_state = useSelector(state => state.DeviceController.data[tile.id], shallowEqual) || {}
+    const [stream_ready, setStreamReady] = useState(false)
+    const video_ref = useRef(null)
+    const dispatch = useDispatch()
+    
+    if(device_state.stream_ready && !stream_ready) {
+        setStreamReady(true)
+    }
+
+    useRtsp(device.id, tile.id)    
+    useRtspPlayStream(stream_ready, video_ref, device.id)
+
+    useEffect(() => {
+        return () => {
+            dispatch(clearTileState(tile.id))
+            setStreamReady(false)
+        }
+    }, [])
+
+    if(device_state.error) {
+        return <div className="center_container">
+            <div className="center_inner">
+                <p>Error: {device_state.message}</p>
+            </div>
+        </div>
+    }
+
+    if(!stream_ready) {
+        return <div className="center_container">
+            <div className="center_inner">
+                <div className="button_loader button_loader_l"></div>
+                <p>Loading Stream...</p>
+            </div>
+        </div>
+    }
+
+    return <video
+        className="videoCanvas"
+        ref={video_ref}
+        autoPlay={true}
+        muted={true}
+        controls={true}
+        style={{marginTop: '10px'}}
+        width="100%"
+        height="auto" />
+}
+
+export default RtspGeneric
